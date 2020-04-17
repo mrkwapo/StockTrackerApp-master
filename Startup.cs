f752f60c1450df87338ee116b87f10660e07cb36
mrkwapo <mrkwapo@gmail.com>
2020-04-16 23:59:32 -0400

    deployment prep

--- Startup.cs
+++ Startup.cs
@@ -55,7 +55,7 @@ namespace MultiUserMVC
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            /*if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseDatabaseErrorPage();
@@ -65,8 +65,8 @@ namespace MultiUserMVC
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
-            }
-            app.UseHttpsRedirection();
+            }*/
+           // app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
